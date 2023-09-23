d796b891785755a14650c50b6e59384925a26ea2
Saeed <saeedbenadeeb@gmail.com>
2023-09-23 17:47:24 +0200

    Updated the Model accuracy class and added some documentations

--- app/models/models.py
+++ app/models/models.py
@@ -1,16 +1,62 @@
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 class ModelAccuracy:
+    """
+    A class for calculating and storing accuracy scores of machine learning models.
+
+    Attributes:
+        models (dict): A dictionary of machine learning models.
+        scaler (StandardScaler): A StandardScaler object used for feature scaling.
+        model_accuracy (dict): A dictionary to store accuracy scores of each model.
+
+    Methods:
+        setup_accuracy(X, Y):
+            Calculate the accuracy scores of machine learning models on a given dataset.
+
+    """
+
     def __init__(self, models, scaler):
+        """
+        Initializes the ModelAccuracy class with machine learning models and a scaler.
+
+        Args:
+            models (dict): A dictionary where keys are model names, and values are machine learning models.
+            scaler (StandardScaler): A StandardScaler object used for feature scaling.
+        """
         self.models = models 
         self.scaler = scaler
         self.model_accuracy = {}
-        self.setup_accuracy()
 
     def setup_accuracy(self, X, Y):
+        """
+        Calculate the accuracy scores of machine learning models on a given dataset.
+
+        Args:
+            X (array-like): The feature matrix.
+            Y (array-like): The target labels.
+
+        Returns:
+            dict: A dictionary containing model names as keys and their accuracy scores as values.
+        """
         for model_name, model in self.models.items():
             # Check if the model is not fitted 
             if not hasattr(model, 'classes_'):
-                if model_name == 'Logistch Regression':
-                    x_train, x_test, y_train, y_test = train_test_split(X, Y, )
+                if model_name == 'Logistic Regression':
+                    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+                elif model_name == 'Random Forest':
+                    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+                elif model_name == 'SVM':
+                    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+                else:
+                    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+                x_train_scaled = self.scaler.fit_transform(x_train)
+                x_test_scaled = self.scaler.transform(x_test)
+
+                model.fit(x_train_scaled, y_train)
+                y_pred = model.predict(x_test_scaled)
+                accuracy = accuracy_score(y_test, y_pred)
+                self.model_accuracy[model_name] = accuracy
+
+        return self.model_accuracy
--- app/services/uploader.py
+++ app/services/uploader.py
@@ -6,13 +6,48 @@ import csv
 from sklearn.preprocessing import StandardScaler
 from flask import request
 
+class UploadHandler:
+    """
+    Handles the processing of uploaded data files, applying a machine learning model for predictions.
 
-class UploadHandler: 
+    Attributes:
+        scaler (StandardScaler): A StandardScaler object used for feature scaling.
+        models (dict): A dictionary of machine learning models.
+
+    Methods:
+        process_upload(file, model_name, cancer_data, x_trained, y_trained):
+            Process an uploaded data file, make predictions using a specified machine learning model,
+            and return the results.
+
+    """
+
     def __init__(self, models):
+        """
+        Initializes the UploadHandler with a StandardScaler and a dictionary of machine learning models.
+
+        Args:
+            models (dict): A dictionary where keys are model names, and values are the corresponding machine learning models.
+        """
         self.scaler = StandardScaler()
         self.models = models
 
     def process_upload(self, file, model_name, cancer_data, x_trained, y_trained):
+        """
+        Process an uploaded data file, make predictions using a specified machine learning model,
+        and return the results.
+
+        Args:
+            file (FileStorage): The uploaded file containing data for prediction.
+            model_name (str): The name of the machine learning model to use for prediction.
+            cancer_data: An object containing cancer data and feature names.
+            x_trained (array-like): The training data features.
+            y_trained (array-like): The training data labels.
+
+        Returns:
+            Union[str, StringIO]: A StringIO object containing the updated CSV data with predictions,
+            or an error message as a string if there are issues with the uploaded data.
+
+        """
         self.x_train = x_trained
         self.x_train_scaled = self.scaler.fit_transform(self.x_train)
         self.y_trained = y_trained
@@ -21,7 +56,7 @@ class UploadHandler:
             # Saving the File 
             file.save(file.filename)
 
-            # Load the uploaded data and check the columns names 
+            # Load the uploaded data and check the column names 
             uploaded_data = pd.read_csv(file.filename)
             uploaded_columns = uploaded_data.columns.tolist()
             uploaded_columns = np.array(uploaded_columns, dtype='<U23')
@@ -29,32 +64,31 @@ class UploadHandler:
                 uploaded_columns = cancer_data.feature_names
             
             if set(uploaded_columns).issubset(set(cancer_data.feature_names)):
-                # Keep only the columns which matchs the original names
+                # Keep only the columns which match the original names
                 try:
                     uploaded_data = uploaded_data[cancer_data.feature_names]
                 except:
-                    return "Uploaded Data Containes Missing Columns"
+                    return "Uploaded Data Contains Missing Columns"
 
-                # Check if There is a Missing Values 
+                # Check if There are Missing Values 
                 if uploaded_data.isnull().values.any():
-                    return "Uploaded Data Containes Missing Values"
+                    return "Uploaded Data Contains Missing Values"
                 
-                # Create a Buffer for the updated csv Data 
+                # Create a Buffer for the updated CSV Data 
                 output_buffer = StringIO()
                 csv_writer = csv.writer(output_buffer)
 
-                # Write the Head Row for the Updated CSV File 
+                # Write the Header Row for the Updated CSV File 
                 uploaded_header = list(uploaded_data.columns) + ['Result']
                 csv_writer.writerow(uploaded_header)
 
-                # Iterate thorugh each row in the uploaded data 
+                # Iterate through each row in the uploaded data 
                 for _, row in uploaded_data.iterrows():
                     features = row.values
                     scaled_feature = self.scaler.transform([features])
                     model = self.models[model_name]
 
-                    # check if the model has been fitted, and fit it if not 
-
+                    # Check if the model has been fitted, and fit it if not 
                     if not hasattr(model, 'classes_'):
                         model.fit(self.x_train_scaled, self.y_trained)
                     
@@ -67,5 +101,4 @@ class UploadHandler:
 
                 return output_buffer
             else:
-                return "Uploaded Data Columns Do Not Match the Original features."
-            
+                return "Uploaded Data Columns Do Not Match the Original Features."
